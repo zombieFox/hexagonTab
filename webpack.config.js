f621182189539b48281985bbe53ff4fae78c971d
zombieFox <crudetoaster@gmail.com>
2021-05-21 12:49:19 +0100

    move to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const ZipPlugin = require('zip-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const version = require('./src/manifest.json').version;
-
 const name = require('./src/manifest.json').name;
 
 module.exports = {
@@ -12,7 +13,7 @@ module.exports = {
     index: path.resolve(__dirname, 'src', 'index.js')
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist/web'),
     clean: true
   },
@@ -22,8 +23,8 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
+      test: /\.css$/i,
+      use: [MiniCssExtractPlugin.loader, 'css-loader'],
     }, {
       test: /\.(ttf|woff|woff2)$/,
       use: {
@@ -33,9 +34,25 @@ module.exports = {
           outputPath: 'font/'
         }
       }
+    }, {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      use: [{
+        loader: 'file-loader',
+      }]
     }]
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin({
+        minify: CssMinimizerPlugin.cleanCssMinify
+      })
+    ]
+  },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
@@ -57,78 +74,3 @@ module.exports = {
     })
   ]
 };
-
-
-
-
-
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const CopyPlugin = require('copy-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const ZipPlugin = require('zip-webpack-plugin');
-// const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-//
-// const version = require('./src/manifest.json').version;
-// const name = require('./src/manifest.json').name;
-//
-// module.exports = {
-//   entry: {
-//     index: path.resolve(__dirname, 'src', 'index.js')
-//   },
-//   output: {
-//     path: path.resolve(__dirname, 'dist/web'),
-//   },
-//   optimization: {
-//     minimize: true
-//   },
-//   plugins: [
-//     new CleanWebpackPlugin(),
-//     new MiniCssExtractPlugin(),
-//     new HtmlWebpackPlugin({
-//       template: path.resolve(__dirname, 'src', 'index.html')
-//     }),
-//     new CopyPlugin({
-//       patterns: [{
-//         from: './src/manifest.json',
-//         to: './manifest.json'
-//       }, {
-//         from: './src/icon/',
-//         to: './icon/'
-//       }, {
-//         from: './src/initialBackground.js',
-//         to: './initialBackground.js'
-//       }]
-//     }),
-//     new ZipPlugin({
-//       path: path.resolve(__dirname, 'dist/extension'),
-//       filename: name + '_' + version + '.zip'
-//     }),
-//     new OptimizeCssAssetsPlugin({
-//       cssProcessorPluginOptions: {
-//         preset: ['default', { discardComments: { removeAll: true } }],
-//       }
-//     })
-//   ],
-//   module: {
-//     rules: [{
-//       test: /\.css$/i,
-//       use: [MiniCssExtractPlugin.loader, 'css-loader']
-//     }, {
-//       test: /\.(ttf|woff|woff2)$/,
-//       use: {
-//         loader: 'file-loader',
-//         options: {
-//           name: '[name].[ext]',
-//           outputPath: 'font/'
-//         },
-//       }
-//     }, {
-//       test: /\.(jpe?g|png|gif|svg)$/i,
-//       use: [{
-//         loader: 'file-loader',
-//       }]
-//     }]
-//   }
-// };
