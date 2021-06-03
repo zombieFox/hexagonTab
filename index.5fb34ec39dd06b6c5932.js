42c001eea063bf4b90ca512955beabb4fbae6a92
zombieFox <crudetoaster@gmail.com>
2021-06-03 08:02:48 +0000

    Deploying to gh-pages from @ zombieFox/hexagonTab@f99c58ccd2eb7405d189651bf070f1ca9e9cf57e 🚀

--- index.5fb34ec39dd06b6c5932.js
+++ index.5fb34ec39dd06b6c5932.js
@@ -14825,6 +14825,8 @@ update_update.all = {
       shades: 9
     };
 
+    dataToUpdate.state.theme.color.lightness.offset = 45 - dataToUpdate.state.theme.color.lightness.contrast;
+
     dataToUpdate.state.theme.color.lightness.start = dataToUpdate.state.theme.color.lightness.offset;
 
     dataToUpdate.state.theme.color.lightness.end = 100 - dataToUpdate.state.theme.color.lightness.offset;
