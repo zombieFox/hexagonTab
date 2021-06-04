48bebeefaef74cd35151ca197205d592cbd30c52
zombieFox <crudetoaster@gmail.com>
2021-06-04 13:12:50 +0100

    refactor menu module

--- src/component/menu/content/bookmark/index.js
+++ src/component/menu/content/bookmark/index.js
@@ -1,34 +1,34 @@
-import { state } from '../../state';
-import { data } from '../../data';
-import { form } from '../../form';
-import { bookmark } from '../../bookmark';
-import { theme } from '../../theme';
-import { toolbar } from '../../toolbar';
-import { grid } from '../../grid';
-import { modal } from '../../modal';
-import { version } from '../../version';
-import { menu } from '../../menu';
-import { icon } from '../../icon';
-import { logo } from '../../logo';
-import { link } from '../../link';
+import { state } from '../../../state';
+import { data } from '../../../data';
+import { form } from '../../../form';
+import { bookmark } from '../../../bookmark';
+import { theme } from '../../../theme';
+import { toolbar } from '../../../toolbar';
+import { grid } from '../../../grid';
+import { modal } from '../../../modal';
+import { version } from '../../../version';
+import { menu } from '../../../menu';
+import { icon } from '../../../icon';
+import { logo } from '../../../logo';
+import { link } from '../../../link';
 
-import { Button } from '../../button';
-import { Collapse } from '../../collapse';
+import { Button } from '../../../button';
+import { Collapse } from '../../../collapse';
 
-import { Control_helperText } from '../../control/helperText';
-import { Control_inputButton } from '../../control/inputButton';
-import { Control_groupText } from '../../control/groupText';
-import { Control_radio } from '../../control/radio';
-import { Control_radioGrid } from '../../control/radioGrid';
-import { Control_checkbox } from '../../control/checkbox';
-import { Control_slider } from '../../control/slider';
-import { Control_slimSlider } from '../../control/slimSlider';
-import { Control_colorMixer } from '../../control/colorMixer';
-import { Control_color } from '../../control/color';
-import { Control_text } from '../../control/text';
+import { Control_helperText } from '../../../control/helperText';
+import { Control_inputButton } from '../../../control/inputButton';
+import { Control_groupText } from '../../../control/groupText';
+import { Control_radio } from '../../../control/radio';
+import { Control_radioGrid } from '../../../control/radioGrid';
+import { Control_checkbox } from '../../../control/checkbox';
+import { Control_slider } from '../../../control/slider';
+import { Control_slimSlider } from '../../../control/slimSlider';
+import { Control_colorMixer } from '../../../control/colorMixer';
+import { Control_color } from '../../../control/color';
+import { Control_text } from '../../../control/text';
 
-import { node } from '../../../utility/node';
-import { complexNode } from '../../../utility/complexNode';
+import { node } from '../../../../utility/node';
+import { complexNode } from '../../../../utility/complexNode';
 
 const menuContentBookmark = {};
 
