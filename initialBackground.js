6fb09002136e0106269bcc03fb70cba48a94aeeb
zombieFox <crudetoaster@gmail.com>
2021-09-26 11:46:48 +0000

    Deploying to gh-pages from @ zombieFox/hexagonTab@71e74c9e19faec114f6c511ea8700d7fa86244a9 🚀

--- initialBackground.js
+++ initialBackground.js
@@ -1,15 +1,1 @@
-// inital check for systems which do not have light or dark mode
-if (localStorage.getItem('hexagonTabStyle')) {
-  var style = document.createElement('style');
-  style.type = 'text/css';
-  style.media = 'screen';
-  switch (localStorage.getItem('hexagonTabStyle')) {
-    case 'light':
-      style.innerHTML = 'html, body {background-color: rgb(255, 255, 255);}';
-      break;
-    case 'dark':
-      style.innerHTML = 'html, body {background-color: rgb(0, 0, 0);}';
-      break;
-  };
-  document.querySelector('head').appendChild(style);
-};
+if(localStorage.getItem("hexagonTabStyle")){const e=document.createElement("style");switch(e.type="text/css",e.media="screen",localStorage.getItem("hexagonTabStyle")){case"light":e.innerHTML="html, body {background-color: rgb(255, 255, 255);}";break;case"dark":e.innerHTML="html, body {background-color: rgb(0, 0, 0);}"}document.querySelector("head").appendChild(e)}
